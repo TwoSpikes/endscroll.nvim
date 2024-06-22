0b4d34cc7921ffda859cae29aadb224d094189f6
Two Spikes <twospikesfpg@gmail.com>
2024-06-22 17:58:46 +0300

    [scroll.lua] If no count, move down by screenline (not by actual line)

--- lua/endscroll/scroll.lua
+++ lua/endscroll/scroll.lua
@@ -4,9 +4,12 @@ local opts = require('endscroll.config').opts
 local scroll_key = api.nvim_replace_termcodes('<C-e>', true, true, true)
 
 local function scroll()
+    local count = vim.v.count
+    local down_key = count == 0 and 'gj' or 'j'
+
     -- check for disabled filetype
     if vim.tbl_contains(opts.disabled_filetypes, vim.o.filetype) then
-        api.nvim_feedkeys('j', 'n', false)
+        api.nvim_feedkeys(down_key, 'n', false)
         return
     end
 
@@ -14,11 +17,11 @@ local function scroll()
     local last_line = fn.line('$')
     local current_line = fn.line('.')
     local top_line = fn.line('w0')
-    local count = vim.v.count1
+    local count1 = vim.v.count1
 
 
 
-    for _ = 1, count, 1 do
+    for _ = 1, count1, 1 do
         local above = api.nvim_win_text_height(0, { start_row = top_line - 1, end_row = current_line - 1}).all - 1
         local below = current_line == last_line and 0 or api.nvim_win_text_height(0, { start_row = current_line - 1, end_row = last_line - 1}).all - 2
 
@@ -32,11 +35,11 @@ local function scroll()
         end
 
         if below < scrolloff and above >= scrolloff and vim.fn.winline() > vim.fn.winheight(vim.fn.winnr()) - scrolloff - 1 then
-            api.nvim_feedkeys('j' .. scroll_key, 'n', false)
+            api.nvim_feedkeys(down_key .. scroll_key, 'n', false)
             goto continue
         end
 
-        api.nvim_feedkeys('j', 'n', true)
+        api.nvim_feedkeys(down_key, 'n', true)
 
         ::continue::
         if current_line < last_line - 1 then
